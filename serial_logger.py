2c35913ad6ab2eb7c11494ec3e4d59d33e20d5e3
samwithbeard <65282597+samwithbeard@users.noreply.github.com>
2025-03-25 12:06:12 +0100

    Update serial_logger.py

--- serial_logger.py
+++ serial_logger.py
@@ -21,7 +21,7 @@ import requests
 from SDMU_parser import parse_ICN_line
 from multiprocessing import Process, Queue
 from gps_collector import start_gps_collector
-print("v6")
+print("v7")
 logging_active=False
 startup_sleep=0
 print("wait "+str(startup_sleep)+"s for startup..")
